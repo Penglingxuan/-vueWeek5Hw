6c99ff81ac47074ea68e820c4b638b80aca0d13a
lingxuan <lingxuan016@cycu.org.tw>
2022-03-09 21:19:17 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,158 @@
+import productModal from './productModal.js';
+
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+
+loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+configure({
+  generateMessage: localize('zh_TW'),
+});
+
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
+const apiPath = 'lingxuan';
+
+Vue.createApp({
+  data() {
+    return {
+      loadingStatus: {
+        loadingItem: '',
+      },
+      products: [],
+      product: {},
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: '',
+      },
+      cart: {},
+    };
+  },
+  components: {
+    VForm: Form,
+    VField: Field,
+    ErrorMessage: ErrorMessage,
+  },
+  methods: {
+    getProducts() {
+      const url = `${apiUrl}/api/${apiPath}/products`;
+      axios.get(url)
+      .then((res) => {
+        this.products = res.data.products;
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    getProduct(id) {
+      const url = `${apiUrl}/api/${apiPath}/product/${id}`;
+      this.loadingStatus.loadingItem = id;
+      axios.get(url)
+      .then((res) => {
+        this.loadingStatus.loadingItem = '';
+        this.product = res.data.product;
+        this.$refs.productModal.openModal();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    addToCart(id, qty = 1) {
+      const url = `${apiUrl}/api/${apiPath}/cart`;
+      this.loadingStatus.loadingItem = id;
+      const cart = {
+        product_id: id,
+        qty,
+      };
+      this.$refs.productModal.hideModal();
+      axios.post(url, { data: cart })
+      .then((res) => {
+        alert(res.data.message);
+        this.loadingStatus.loadingItem = '';
+        this.getCart();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    updateCart(data) {
+      this.loadingStatus.loadingItem = data.id;
+      const url = `${apiUrl}/api/${apiPath}/cart/${data.id}`;
+      const cart = {
+        product_id: data.product_id,
+        qty: data.qty,
+      };
+      axios.put(url, { data: cart })
+      .then((res) => {
+        alert(res.data.message);
+        this.loadingStatus.loadingItem = '';
+        this.getCart();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+        this.loadingStatus.loadingItem = '';
+      });
+    },
+    deleteAllCarts() {
+      const url = `${apiUrl}/api/${apiPath}/carts`;
+      axios.delete(url)
+      .then((res) => {
+        alert(res.data.message);
+        this.getCart();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    getCart() {
+      const url = `${apiUrl}/api/${apiPath}/cart`;
+      axios.get(url)
+      .then((res) => {
+        this.cart = res.data.data;
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    removeCartItem(id) {
+      const url = `${apiUrl}/api/${apiPath}/cart/${id}`;
+      this.loadingStatus.loadingItem = id;
+      axios.delete(url)
+      .then((res) => {
+        alert(res.data.message);
+        this.loadingStatus.loadingItem = '';
+        this.getCart();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+    createOrder() {
+      const url = `${apiUrl}/api/${apiPath}/order`;
+      const order = this.form;
+      axios.post(url, { data: order })
+      .then((res) => {
+        alert(res.data.message);
+        this.$refs.form.resetForm();
+        this.getCart();
+      })
+      .catch((err) => {
+        alert(err.data.message);
+      });
+    },
+  },
+  mounted() {
+    this.getProducts();
+    this.getCart();
+  },
+}).component('productModal', productModal).mount('#app');
